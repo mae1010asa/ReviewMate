993f136cf11264a940e0ebcfb2c3901b507a94b8
Ubuntu <ubuntu@ip-172-31-26-254.ap-northeast-1.compute.internal>
2025-11-04 08:47:52 +0000

    トップページ・devise実装

---
+++ db/migrate/20251104065956_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+
+      t.string :title, null: false
+      t.text :body, null: false
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20251104070010_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.string :body, null: false
+      t.string :star, null: false
+      t.timestamps
+    end
+  end
+end

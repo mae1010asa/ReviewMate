62245073b891ad18e0d8417b6ef8174b8d317b45
mae1010asa <229768141+mae1010asa@users.noreply.github.com>
2025-12-14 09:52:36 +0000

    コメントに対するいいね

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,14 @@ class Public::FavoritesController < ApplicationController
     comment = Comment.find(params[:comment_id])
     favorite = current_user.favorites.new(comment_id: comment.id)
     favorite.save
-    redirect_to item_review_path(review.item, review)
+    redirect_to request.referer
   end
 
   def destroy
     comment = Comment.find(params[:comment_id])
     favorite = current_user.favorites.find_by(comment_id: comment.id)
     favorite.destroy
-    redirect_to item_review_path(review.item, review)
+    redirect_to request.referer
   end
 
 end

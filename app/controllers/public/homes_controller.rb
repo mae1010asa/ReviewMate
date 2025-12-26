79031d2b65a9ea5c91b3feaf997f98f47d6a3ac9
mae1010asa <229768141+mae1010asa@users.noreply.github.com>
2025-12-26 22:38:21 +0000

    トップページに最新のレビュー一覧を追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @q = Review.ransack(params[:q])
+    @reviews = Review.all.order(created_at: :desc).includes(:item)
   end
   def about
   end
